6e5435c241ae9c914e0bc9ccb33ec3250b74da31
j-seibel <107728907+j-seibel@users.noreply.github.com>
2023-09-23 16:05:35 -0500

    Added recognition model and file upload endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from face_recognizer.detector import add_image
 from dotenv import dotenv_values
 from pymongo.mongo_client import MongoClient
 from routes import router as user_router
@@ -17,4 +18,13 @@ def startup_db_client():
 def shutdown_db_client():
     app.mongodb_client.close()
 
+@app.get("/api/v1/profile/upload")
+async def upload_image(image: UploadFile):
+
+    add_image(image.file)
+    return {"message": "Image uploaded successfully"}
+
 app.include_router(user_router, tags=["users"], prefix="/users")
+
+
+
