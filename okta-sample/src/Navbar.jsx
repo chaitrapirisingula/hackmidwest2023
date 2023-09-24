86d6929e8ecb6e629f1d02dc05211b449f658b43
chaitrapirisingula <89547692+chaitrapirisingula@users.noreply.github.com>
2023-09-23 20:49:18 -0500

    lots of front end

---
+++ okta-sample/src/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useOktaAuth } from '@okta/okta-react';
+import { Link } from 'react-router-dom';
+import { Container, Icon, Image, Menu } from 'semantic-ui-react';
+import logo from './logo.png';
+
+const Navbar = () => {
+  const { authState, oktaAuth } = useOktaAuth();
+
+  const login = async () => oktaAuth.signInWithRedirect();
+  const logout = async () => oktaAuth.signOut();
+
+  if (!authState) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Menu fixed="top" inverted>
+        <Container>
+          <Menu.Item header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Link to="/">Saving Face</Link>
+          </Menu.Item>
+          {authState.isAuthenticated && (
+            <Menu.Item id="profile-button">
+              <Link to="/profile">Profile</Link>
+            </Menu.Item>
+          )}
+          {authState.isAuthenticated && (
+            <Menu.Item id="logout-button" onClick={logout}>Logout</Menu.Item>
+          )}
+          {!authState && !authState.isAuthenticated && (
+            <Menu.Item onClick={login}>Login</Menu.Item>
+          )}
+        </Container>
+      </Menu>
+    </div>
+  );
+};
+export default Navbar;
