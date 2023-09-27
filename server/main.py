43dd78e962eb2d10a99af4b2272c046a6cc00160
chaitrapirisingula <89547692+chaitrapirisingula@users.noreply.github.com>
2023-09-27 11:57:28 -0500

    rename server

---
+++ server/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, File, UploadFile
+from dotenv import dotenv_values
+from pymongo.mongo_client import MongoClient
+from routes import router as user_router
+import certifi
+from fastapi.middleware.cors import CORSMiddleware
+
+config = dotenv_values(".env")
+
+app = FastAPI()
+
+origins = [
+    '*'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["ATLAS_URI"], tlsCAFile=certifi.where())
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
+
+app.include_router(user_router, tags=["users"], prefix="/users")
